aefcccd93319ac8d2b12789ba3c944159a0d071e
Nikolay <nikolay.kovzik@gmail.com>
2022-01-19 03:04:33 +0300

    feat: add mathRandom variants

--- learnJavaScript/arrayMethods/calculator.js
+++ learnJavaScript/arrayMethods/calculator.js
@@ -44,3 +44,33 @@ console.log( powerCalc.calculate("3 ** 4") );
 console.log( powerCalc.calculate("2 - 1") ); 
 console.log( powerCalc.calculate("12 / 3") ); 
 console.log( powerCalc.calculate("17 * 2") ); 
+
+
+
+/*
+function Calculator() {
+
+  this.methods = {
+    "-": (a, b) => a - b,
+    "+": (a, b) => a + b
+  };
+
+  this.calculate = function(str) {
+
+    let split = str.split(' '),
+      a = +split[0],
+      op = split[1],
+      b = +split[2]
+
+    if (!this.methods[op] || isNaN(a) || isNaN(b)) {
+      return NaN;
+    }
+
+    return this.methods[op](a, b);
+  }
+
+  this.addMethod = function(name, func) {
+    this.methods[name] = func;
+  };
+}
+*/
