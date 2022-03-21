0e2e8abb1f693ba45725d4a0c2df467f5d0ec921
Nikolay <nikolay.kovzik@gmail.com>
2022-03-22 00:39:52 +0300

    RLE/recycleBin/jsMathematician CW stage 1

---
+++ codeWars/stage1/RLE.js
@@ -0,0 +1,29 @@
+// Run-length encoding (RLE) is a very simple form of data compression in which runs of data (that is, 
+// sequences in which the same data value occurs in many consecutive data elements) are stored as
+//  a single data value and count, rather than as the original run. Wikipedia
+
+// Task
+// Your task is to write such a run-length encoding.
+
+// Examples
+// As the article states, RLE is a very simple form of data compression.
+// It's only suitable for runs of data, as one can see in the following example:
+
+// runLengthEncoding("hello world!")
+//  =>      [[1,'h'], [1,'e'], [2,'l'], [1,'o'], [1,' '], [1,'w'], [1,'o'], [1,'r'], [1,'l'], [1,'d'], [1,'!']]
+
+// runLengthEncoding("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbb")
+//   => [[34,'a'], [3,'b']]
+
+
+var runLengthEncoding = function (str) {
+
+    let result = [];
+    while (str) {
+        let coincidence = str.match(/(.)\1*/);
+        result.push([coincidence[0].length, coincidence[1]]);
+        str = str.slice(coincidence[0].length);
+    }
+
+    return result
+}
---
+++ codeWars/stage1/jsMathematician.js
@@ -0,0 +1,20 @@
+// You are writing a function that takes two sets of arguments of arbitrary length. 
+// The return value will be the sum of the values of all of the arguments.
+// The function should contain at least 1 argument per set.
+
+// calculate(1)(1) // should return 2
+// calculate(1,1)(1) // should return 3
+// calculate(1,1)(1,-1) // should return 2
+// calculate(2,4)(3,7,1) // should return 17
+
+function calculate(...args1) {
+    return function(...args2){
+      return args1.concat(args2).reduce((res,curr)=>{return res+=curr},0)
+    };
+}
+
+
+//best practice
+/*
+const calculate = (...args1) => (...args2) => [...args1, ...args2].reduce((s, v) => s + v);
+*/
