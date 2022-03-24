0f269b65a93f4e9798c74354569692a021809f69
Nikolay <nikolay.kovzik@gmail.com>
2022-03-24 04:13:09 +0300

    humanReadableDurationFormat fix

--- codeWars/stage1/humanReadableDurationFormat.js
+++ codeWars/stage1/humanReadableDurationFormat.js
@@ -28,86 +28,36 @@
 
 
 
-function secondsCalc(seconds) {
-  return (seconds == 0) ? 'now' : (seconds == 1) ? `1 second` : `${seconds} seconds`
-}
-
-function minutesCalc(minutes, seconds) {
-  let res = (minutes == 1) ? `1 minute` : `${minutes} minutes`
-  if (seconds) {
-    res += ` and ${secondsCalc(seconds)}`
-  }
-  return res;
-}
-
-function hoursCalc(hours, minutes, seconds) {
-  let res = (hours == 1) ? `1 hour` : `${hours} hours`
-  if (minutes) {
-    res += (seconds) ? `, ${minutesCalc(minutes, seconds)}` : ` and ${minutesCalc(minutes, seconds)}`
-  } else if (seconds) {
-    res += ` and ${secondsCalc(seconds)}`
-  }
-  return res;
-}
-
-function daysCalc(days, hours, minutes, seconds) {
-  let res = (days == 1) ? `1 day` : `${days} days`
-  if (hours) {
-    res += (minutes||seconds) ? `, ${hoursCalc(hours, minutes, seconds)}` : ` and ${hoursCalc(hours, minutes, seconds)}`
-  } else if (minutes) {
-    res += (seconds) ? `, ${minutesCalc(minutes, seconds)}` : ` and ${minutesCalc(minutes, seconds)}`
-  } else if (seconds) {
-    res += ` and ${secondsCalc(seconds)}`
-  }
-  return res;
-}
-
-function yearsCalc(years, days, hours, minutes, seconds) {
-  let res = (years == 1) ? `1 year` : `${years} years`
-  if (days) {
-    res += (hours||minutes||seconds) ? `, ${daysCalc(days, hours, minutes, seconds)}` : ` and ${daysCalc(days, hours, minutes, seconds)}`
-  } else if (hours) {
-    res += (minutes||seconds) ? `, ${hoursCalc(hours, minutes, seconds)}` : ` and ${hoursCalc(hours, minutes, seconds)}`
-  } else if (minutes) {
-    res += (seconds) ? `, ${minutesCalc(minutes, seconds)}` : ` and ${minutesCalc(minutes, seconds)}`
-  } else if (seconds) {
-    res += ` and ${secondsCalc(seconds)}`
-  }
-  return res;
-}
-
-function formatDuration(seconds) {
-  if (seconds < 60) {
-    return secondsCalc(seconds);
-  }
-
-  let minutes = Math.floor(seconds / 60);
-  seconds = seconds % 60;
-
-  if (minutes < 60) {
-    return minutesCalc(minutes, seconds)
-  }
-
-  let hours = Math.floor(minutes / 60);
-  minutes = minutes % 60;
-
-  if (hours < 24) {
-    return hoursCalc(hours, minutes, seconds)
-  }
-
-  let days = Math.floor(hours / 24);
-  hours = hours % 24;
-
-  if (days < 365) {
-    return daysCalc(days, hours, minutes, seconds)
-  }
-
-  let years = Math.floor(days / 365);
-  days = days % 365;
-
-  return yearsCalc(years, days, hours, minutes, seconds)
+function CreateTimeObject(timestamp) {
+  let seconds = timestamp % 60;
+  let minutes = Math.floor(timestamp / 60) % 60;
+  let hours = Math.floor(timestamp / 3600) % 24;
+  let days = Math.floor(timestamp / 86400) % 365;
+  let years = Math.floor(timestamp / 31536000);
+  if (years) this[`year${years == 1 ? '' : 's'}`] = years;
+  if (days) this[`day${days == 1 ? '' : 's'}`] = days;
+  if (hours) this[`hour${hours == 1 ? '' : 's'}`] = hours;
+  if (minutes) this[`minute${minutes == 1 ? '' : 's'}`] = minutes;
+  if (seconds) this[`second${seconds == 1 ? '' : 's'}`] = seconds;
 }
 
 
 
-console.log(formatDuration(8643600))
+function formatDuration(timestamp) {
+
+  if (!timestamp) return 'now';
+
+  let time = new CreateTimeObject(timestamp)
+  let res;
+  if (Object.keys(time).length == 1) {
+    return `${Object.values(time)[0]} ${Object.keys(time)[0]}`
+  } else {
+    return Object.entries(time).map((entry) => {
+      return entry.reverse().join(' ')
+    }).join(', ').replace(/(, )([^,]*)?$/, ' and '+'$2')
+  }
+}
+
+
+console.log(formatDuration(60*60 + 60))
+console.log(formatDuration(1222222))
